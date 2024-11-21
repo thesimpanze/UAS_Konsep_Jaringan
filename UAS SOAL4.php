2205ed3b2315a0d51d49b238523a031868a6b3aa
thesimpanze <rasyid8nuruddin@gmail.com>
2024-11-21 11:55:30 +0700

    Add files via upload

---
+++ UAS SOAL4.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Biodata</title>
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 50%;
+            margin: 0 auto;
+        }
+        th, td {
+            border: 1px solid black;
+            padding: 8px;
+            text-align: left;
+        }
+        th {
+            text-align: center;
+            font-weight: bold;
+        }
+        .footer {
+            text-align: center;
+            margin-top: 20px;
+            font-weight: bold;
+        }
+        img {
+            display: block;
+            margin: 10px auto;
+            width: 150px; /* Sesuaikan ukuran foto */
+            height: auto;
+        }
+    </style>
+</head>
+<body>
+    <table>
+        <tr >
+            <td colspan="3"><b>BIODATA</b></td>
+        </tr>
+        <tr>
+            <td rowspan="4">
+                <img src="IMG_6465.jpg" alt="Foto Biodata">
+            </td>
+            <td>Nama</td>
+            <td>: Muhammad Rasyid Nuruddin</td>
+        </tr>
+        <tr>
+            <td>Alamat</td>
+            <td>: Jombang</td>
+        </tr>
+        <tr>
+            <td>Email</td>
+            <td>: <a href="mailto:rasyid8nuruddin@it.student.pens.ac.id">rasyid8nuruddin@it.student.pens.ac.id</a></td>
+        </tr>
+        <tr>
+            <td>IG</td>
+            <td>: <a href="http://instagram.com/whorsyd" target="_blank">http://instagram.com/whorsyd</a></td>
+        </tr>
+        <tr >
+            <td colspan="3" style="text-align: center;">- Iseng4h -</td>
+        </tr>
+    </table>
+    <div class="footer">
+        
+    </div>
+</body>
+</html>
